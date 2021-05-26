67250bb82cb75b9db52368b3a5bfe45dbe7ba0c9
ezazulhaq <61641801+ezazulhaq@users.noreply.github.com>
2021-05-26 23:40:02 +0530

    missing getChats methods added in view models

--- lib/viewmodels/chats_view_model.dart
+++ lib/viewmodels/chats_view_model.dart
@@ -1,6 +1,7 @@
 // @dart=2.9
 import 'package:chat/chat.dart';
 import 'package:chat_app/data/datasources/datasource_contract.dart';
+import 'package:chat_app/models/chat.dart';
 import 'package:chat_app/models/local_message.dart';
 import 'package:chat_app/viewmodels/base_view_model.dart';
 
@@ -8,6 +9,10 @@ class ChatsViewModel extends BaseViewModel {
   // ignore: unused_field
   IDataSource _dataSource;
   ChatsViewModel(IDataSource _datasource) : super(_datasource);
+
+  Future<List<Chat>> getChats() async {
+    return await _dataSource.findAllChats();
+  }
 
   Future<void> receivedMessage(Message message) async {
     LocalMessage localMessage =
